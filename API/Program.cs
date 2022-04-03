7d9a21b6ab006bc23151bf4ba2c147c2686273ac
baronoflulz <baronoflulz@gmail.com>
2022-04-03 23:00:00 +0300

    Added: using JWT token to Authenticate Reqs, cleanup.

--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,25 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddCors();
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI
 // at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlite(builder.Configuration
-        .GetConnectionString("DefaultConnection")));
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 
@@ -33,6 +36,7 @@ app.UseCors(policy => policy.AllowAnyHeader()
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
