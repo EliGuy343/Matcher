d5fa1d61615df7127c005dce936c93990259c73c
baronoflulz <baronoflulz@gmail.com>
2022-04-06 14:02:07 +0300

    Added: routing, Toast messages

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import {Observable } from 'rxjs';
 import { User } from '../models/user';
 import { AccountService } from '../services/account.service';
@@ -12,21 +14,26 @@ export class NavComponent implements OnInit {
   model:any = {};
 
 
-  constructor(public accountService:AccountService) {}
+  constructor(public accountService:AccountService,
+    private router:Router, private toastr:ToastrService) {}
   
   ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: res=>{
-        console.log(res);
+        this.router.navigateByUrl("/members");
       },
-      error: error=>console.log(error) 
+      error: error=> {
+        this.toastr.error(error.error);
+        console.log(error);
+      } 
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 
 }
