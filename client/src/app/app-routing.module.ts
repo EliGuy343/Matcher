d70699562720ba0187928263fad1ec6c86cf7cef
baronoflulz <baronoflulz@gmail.com>
2022-04-16 19:33:25 +0300

    Created: user edit page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,6 +11,8 @@ import { AuthGuard } from './guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { PreventUnsavedChangesGuard } from './guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
@@ -19,7 +21,7 @@ const routes: Routes = [
     runGuardsAndResolvers:'always',
     canActivate:[AuthGuard],
     children: [
-      {path:'members', component:MemberListComponent, canActivate:[AuthGuard]},
+      {path:'members', component:MemberListComponent},
       {path:'members/:username', component:MemberDetailComponent},
       {path:'lists', component:ListsComponent},
       {path:'messages', component: MessagesComponent},
@@ -28,6 +30,7 @@ const routes: Routes = [
   {path:'errors', component:TestErrorsComponent},
   {path:'not-found', component:NotFoundComponent},
   {path:'server-error', component:ServerErrorComponent},
+  {path:'member/edit', component:MemberEditComponent, canDeactivate:[PreventUnsavedChangesGuard]},
   {path:'**', component: NotFoundComponent, pathMatch:'full'}
 ];
 
