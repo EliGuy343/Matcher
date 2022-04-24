55cd0c1b7434cc10d27424894c98364fcbcb4822
baronoflulz <baronoflulz@gmail.com>
2022-04-24 12:28:29 +0300

    Added: backend Pagination

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -30,8 +31,15 @@ namespace API.Controllers
         }
         
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> getUsers() =>
-            Ok(await _userRepository.GetMembersAsync());
+        public async Task<ActionResult<IEnumerable<MemberDto>>> getUsers([FromQuery]UserParams userParams)
+        {
+            var users = await _userRepository.GetMembersAsync(userParams);
+            Response.AddPaginationHeader(users.CurrentPage,
+                users.PageSize, users.TotalCount, users.TotalPages);
+            
+            return Ok(users);
+        }
+            
         
         
         [HttpGet("{username}",Name = "GetUser")]
